382a16289cea40c0322a5e31d20595e1483a790f
Mariya_Zaya <mariyazayaschnikova@gmail.com>
2023-02-20 19:05:43 +0300

    refactoring code, changed style

--- src/components/pages/menu-item-page.js
+++ src/components/pages/menu-item-page.js
@@ -1,44 +1,42 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import WithRestoService from '../hoc/with-resto-service';
-import {menuLoaded, menuRequested, isError, itemAddToCart} from '../../actions/index';
+import { menuLoaded, menuRequested, isError, itemAddToCart } from '../../actions/index';
 import Spinner from '../spinner/spinner';
 import Error from '../error/error';
 import './menu-item-page.scss';
 
-class MenuItem extends Component{
+class MenuItem extends Component {
     componentDidMount() {
         this.props.menuRequested();
 
-        let {RestoService} = this.props;
+        let { RestoService } = this.props;
         RestoService.getMenuItems()
             .then(res => this.props.menuLoaded(res))
             .catch(() => this.props.isError());
     }
 
     render() {
-        let {menuItems, loading, error, itemAddToCart} = this.props;
+        let { menuItems, loading, error, itemAddToCart } = this.props;
 
-        if(error) {
-            return <Error/>
-        }
+        if (error) return <Error />;
 
-        if(loading) {
-            return <Spinner/>
-        }
+        if (loading) return <Spinner />;
 
         let id = +this.props.match.params.id;
 
         let item = menuItems.find(item => id === +item.id);
-        let {title, price, url, category} = item;
-        
+        let { title, price, url, category } = item;
+
         return (
-            <div className="menu_item">
+            <div className="menu_container">
+                <div className="menu_item">
                     <div className="menu_title">{title}</div>
                     <img className="menu_img" src={url} alt={title}></img>
                     <div className="menu_category">Category: <span>{category}</span></div>
                     <div className="menu_price">Price: <span>{price}$</span></div>
                     <button onClick={() => itemAddToCart(id)} className="menu_btn">Add to cart</button>
+                </div>
             </div>
         )
     }
@@ -53,10 +51,10 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = {
-        menuLoaded,
-        menuRequested,
-        isError,
-        itemAddToCart
+    menuLoaded,
+    menuRequested,
+    isError,
+    itemAddToCart
 };
 
 export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuItem));
